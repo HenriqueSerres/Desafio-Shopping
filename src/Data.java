07d8b60a8fb18db0ce2938965f3023662226e4a1
Henrique Serres <henriqueserres@gmail.com>
2023-04-18 22:25:46 -0300

    Etapa 1 do desafio completada!

---
+++ src/Data.java
@@ -0,0 +1,86 @@
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+        verifyDate();
+    }
+    private void verifyDate() {
+        if (!dateIsValid()) {
+            System.out.println("Data inválida!!");
+            dia = 1;
+            mes = 1;
+            ano = 2000;
+        }
+    }
+    public boolean dateIsValid() {
+        if (verificaAnoBissexto() && mes == 2 && dia <= 29) {
+            return true;
+        } else if (mes == 2 && dia >= 1 && dia > 28) {
+            System.out.println("Fevereiro vai até o dia 28!");
+            return false;
+        } else if (verificaMes30() && dia >= 1 && dia <= 30) {
+            return true;
+        } else if (verificaMes31() && dia >= 1 && dia <= 31) {
+            return true;
+        }
+        return false;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public String toString() {
+        return dia+"/"+mes+"/"+ano;
+    }
+    public boolean verificaAnoBissexto () {
+        if(ano % 4 == 0) {
+            return true;
+        }
+        return false;
+    }
+    public boolean verificaMes30() {
+        int[] meses = {2,4,6,9,11};
+        boolean areHere = false;
+        for (int i = 0; i < meses.length; i++) {
+            if (meses[i] == mes) {
+                areHere = true;
+            }
+        }
+        return areHere;
+    }
+    public boolean verificaMes31() {
+        int[] meses = {1,3,5,7,8,11};
+        boolean areHere = false;
+        for (int i = 0; i < meses.length; i++) {
+            if (meses[i] == mes) {
+                areHere = true;
+            }
+        }
+        return areHere;
+    }
+}
---
+++ src/Loja.java
@@ -0,0 +1,50 @@
+public class Loja {
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+    public Loja(String nome, int quantidadeFuncionarios) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        salarioBaseFuncionario = -1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+
+    public double getSalarioBaseFuncionario() {
+        return salarioBaseFuncionario;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+    public String toString() {
+        return "Nome da loja: "+getNome()+". Possui "+getQuantidadeFuncionarios()+" funcionários. Com base salarial de R$ "+getSalarioBaseFuncionario();
+    }
+    public double gastosComSalario() {
+        if (salarioBaseFuncionario == -1) return -1;
+        return salarioBaseFuncionario * quantidadeFuncionarios;
+    }
+    public char tamanhoDaLoja() {
+        if (quantidadeFuncionarios < 10) return 'P';
+        else if (quantidadeFuncionarios <= 30) return 'M';
+        else return 'G';
+    }
+}
